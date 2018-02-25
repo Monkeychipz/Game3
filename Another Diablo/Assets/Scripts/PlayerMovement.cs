2ba78b39e36bb30ef4cc6da1c5f11856cfd0d64d
redderpanda <jaredsc@uci.edu>
2018-02-24 20:38:12 -0800

    FUCKING WORK DAMNIT
    hello

---
+++ Another Diablo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D rb2d;
+    public float speed;
+
+    void Start() {
+        //currenthealth = health;
+        rb2d = GetComponent<Rigidbody2D>();
+        //speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        PlayerRotation();
+        rb2d.transform.position += new Vector3(Input.GetAxis("Left Joystick Horizontal"), -Input.GetAxis("Left Joystick Vertical"), 0) * Time.deltaTime * speed;
+    
+    }
+
+    void PlayerRotation()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+
+        transform.up = direction;
+    }
+
+    
+}
---
+++ Another Diablo/Assets/Scripts/WarriorController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorController : MonoBehaviour {
+    // class that utilizes the general Player Movement script 
+    // set a speed for the player in the Start() 
+    public float health;
+    public float rage;
+    public bool isBasicAttacking;
+    public PlayerMovement player_movement;
+
+	// Use this for initialization
+	void Start () {
+        health = 100f;
+        rage = 100f;
+        isBasicAttacking = false;
+        player_movement.speed = 5f;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(BasicAttack());  
+		
+	}
+
+    private IEnumerator BasicAttack()
+    {
+        if (Input.GetAxis("Right Trigger") == 1)
+        {
+            if (!isBasicAttacking)
+            {
+                isBasicAttacking = true;
+                Debug.Log("basic attack");
+                yield return new WaitForSeconds(1f);
+                isBasicAttacking = false;
+
+            }
+        }
+    }
+    private void ShieldBash() //mobility skill: that knocks back and stuns if hits a wall
+    {
+
+    }
+    private void Taunt() // utility skill: AOE taunt around the player
+    {
+
+    }
+    private void Cleave() // some time of AOE damage around the player
+    {
+
+    }
+    private void Berserker()// Ultimate skill: cant be CC'd (maybe also attack speed up)
+    {
+
+    }
+}
