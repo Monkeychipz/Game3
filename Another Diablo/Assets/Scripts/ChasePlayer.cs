9b9df406ff18f2b51d14d59627274bf7ea43c002
Monkeychipz <34406624+Monkeychipz@users.noreply.github.com>
2018-02-24 20:56:55 -0800

    fuck you git

---
+++ Another Diablo/Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    private Transform player = null;
+    bool inRange = false;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange && player != null)
+        {
+            transform.position += (player.position - transform.position).normalized * speed * Time.deltaTime;
+            Vector3 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 270;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            inRange = true;
+            player = collision.transform;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = null;
+        }
+    }
+}
